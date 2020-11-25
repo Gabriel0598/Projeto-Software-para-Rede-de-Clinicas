1fd809a6df8b1338bd1f0f4775c76bee537fa4b1
Gabriel <augustogabriel35@gmail.com>
2020-11-25 12:25:27 -0300

    P3 - Codigo base/ Tela para consulta de pagamentos registrados

---
+++ Tela_Inicial/Main_7_R.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main()
+{
+    //Consulta de pagamentos realizados
+    setlocale(LC_ALL, "Portuguese"); //Conversão de língua para português
+
+    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\Código Fonte PIM\\Tela_Inicial\\Lista_de_Pagamentos.txt\"");
+    printf("\n\t\t\t\t\t\t\t\t\tCONSULTA DE PAGAMENTOS REALIZADOS\n\n");
+    printf("\t\t\t\t\t\t\t\t   ***DIGITE ENTER PARA RETORNAR AO MENU***\n\n\n\n");
+    getch();
+    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\Código Fonte PIM\\Tela_Inicial\\Main_0.exe\"");
+    return(0);
+}
