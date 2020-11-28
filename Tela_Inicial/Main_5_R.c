f5a73f0a033e6bd2e4984f252ed2375e3a63cb0e
Gabriel <augustogabriel35@gmail.com>
2020-11-28 18:22:35 -0300

    P10 - Migracao de pasta - Exames/ Definitivo

--- Tela_Inicial/Main_5_R.c
+++ Tela_Inicial/Main_5_R.c
@@ -6,7 +6,7 @@ int main()
     //Consulta de exames agendados
     setlocale(LC_ALL, "Portuguese"); //Conversão de língua para português
 
-    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\Código Fonte PIM\\Tela_Inicial\\Lista_De_Exames.txt\"");
+    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\UNIP\\2° Semestre\\PIM 2 Sem\\Código Fonte PIM\\Tela_Inicial\\Lista_De_Exames.txt\"");
     printf("\n\t\t\t\t\t\t\t\t\tCONSULTA DE EXAMES AGENDADOS\n\n");
     printf("\t\t\t\t\t\t\t\t***DIGITE ENTER PARA RETORNAR AO MENU***\n\n\n\n");
     getch();
