89b4e9d726d2d054ee626b9f00bca362fb325ddd
Gabriel <augustogabriel35@gmail.com>
2020-11-28 18:21:25 -0300

    P8 - Migracao de pasta - Funcionarios/ Definitivo

--- Tela_Inicial/Main_1_R.c
+++ Tela_Inicial/Main_1_R.c
@@ -6,7 +6,7 @@ int main()
     //Consulta de funcionários
     setlocale(LC_ALL, "Portuguese"); //Conversão de língua para português
 
-    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\Código Fonte PIM\\Tela_Inicial\\Ficha_Funcionario.txt\"");
+    system("\"C:\\Users\\Gabri\\OneDrive\\Documentos\\UNIP\\2° Semestre\\PIM 2 Sem\\Código Fonte PIM\\Tela_Inicial\\Ficha_Funcionario.txt\"");
     printf("\n\t\t\t\t\t\t\t\t\tCONSULTA DE FUNCIONÁRIOS\n\n");
     printf("\t\t\t\t\t\t\t\t***DIGITE ENTER PARA RETORNAR AO MENU***\n\n\n\n");
     getch();
